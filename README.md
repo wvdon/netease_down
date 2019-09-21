e462ed2c10730f8bff0d4ffd7b29d040a6867a9c
wvdon <mail@wvdon.com>
2019-09-21 17:57:31 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Netease_down
-通过链接下载网易云音乐歌单的所有音乐
+> Version 1.0
+
+可以通过链接下载网易云音乐歌单的所有音乐 
+
+clone或者直接下载到本地
+
+```
+git clone https://github.com/wvdon/netease_down.git
+```
+
+安装需要的包
+
+```
+pip install -r requests.txt
+```
+
+运行下载
+
+```
+python netease.py
+```
+
+输入你要下载的歌单id :
+
+```
+输入你要爬取歌单的id： 2884035
+解析完成
+获取界面完成
+开始下载第0首歌愿你有故乡.mp3
+http://music.163.com/song/media/outer/url?id=1390298115.mp3
+下载愿你有故乡.mp3完成
+开始下载第1首歌推理.mp3
+http://music.163.com/song/media/outer/url?id=1390484461.mp3
+下载推理.mp3完成
+开始下载第2首歌下雨的杭州.mp3
+http://music.163.com/song/media/outer/url?id=1391433967.mp3
+下载下雨的杭州.mp3完成
+开始下载第3首歌狗狗诗人.mp3
+http://music.163.com/song/media/outer/url?id=1390484118.mp3
+下载狗狗诗人.mp3完成
+······················
+```
+
+
+
---
+++ netease.py
@@ -0,0 +1,58 @@
+import requests
+from lxml import etree
+import os
+class netease(object):
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
+
+        }
+        self.url = input("输入你要爬取歌单的id： ")
+        self.url = "https://music.163.com/discover/toplist?id="+self.url
+        #self.url = "https://music.163.com/discover/toplist?id=3778678"
+        self.url_list=[]
+        self.name_list = []
+    def getPage(self):
+        res =requests.get(self.url,headers=self.headers).text
+        html= etree.HTML(res)
+        self.parsePage(html)
+        print("获取界面完成")
+    def parsePage(self,html):
+
+        urls = html.xpath('//ul[@class="f-hide"]/li/a/@href')
+        names=html.xpath('//ul[@class="f-hide"]/li/a/text()')
+
+        for i in range(0,len(urls)):
+            url = "http://music.163.com/song/media/outer/url" + str(urls[i][5:]) + ".mp3"
+            self.url_list.append(url)
+            name=str(names[i])+".mp3"
+            self.name_list.append(name)
+        print("解析完成")
+    def show(self,contents):
+        for content in contents:
+            print(content)
+
+    def downEase(self,url,name):
+        print(url)
+        music = requests.get(url,headers=self.headers)
+        path1 = r'./music/'
+        path=path1+str(name)
+        with open(path,"wb") as m:
+             m.write(music.content)
+             m.close()
+        print("下载%s完成"%name)
+
+        pass
+    def workOn(self):
+        self.getPage()
+        for i in range(0,len(self.name_list)):
+            print("开始下载第%d首歌%s"%(i,self.name_list[i]))
+            self.downEase(self.url_list[i],self.name_list[i])
+if __name__ == '__main__':
+    #如果不存在的时候创建存储的文件夹 music
+   if os.path.exists('./music/')==False:
+        path1 = r'./music/'
+        os.mkdir(path1)
+   splider=netease()
+   splider.workOn()
+   print("全部下载完成")
